4157e82fe09f7b915ed58563d3d528cfe4e55a6b
xojiakbar <obboooo@mail.ru>
2019-07-15 18:04:24 +0500

    refactor ImageWithTextButton

--- app/src/main/java/uz/zokirbekov/gplay/MainActivity.kt
+++ app/src/main/java/uz/zokirbekov/gplay/MainActivity.kt
@@ -2,14 +2,26 @@ package uz.zokirbekov.gplay
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.widget.FrameLayout
+import uz.zokirbekov.gplay.fragments.MainMenuFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var content:FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        content = findViewById(R.id.content)
+        switchFragment(MainMenuFragment())
     }
 
-
+    public fun switchFragment(fragment: Fragment)
+    {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.content,fragment)
+                .commit()
+    }
 
 }
--- app/src/main/java/uz/zokirbekov/gplay/ui/ImageWithTextButton.kt
+++ app/src/main/java/uz/zokirbekov/gplay/ui/ImageWithTextButton.kt
@@ -2,14 +2,36 @@ package uz.zokirbekov.gplay.ui
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import uz.zokirbekov.gplay.R
 
 class ImageWithTextButton(context: Context, attr:AttributeSet) : FrameLayout(context, attr) {
 
-    lateinit var image:ImageView
-    lateinit var text:TextView
+    lateinit var imageView:ImageView
+    lateinit var textView:TextView
 
+    init {
+        initViews()
+        setupAttributes(attr)
+    }
+
+    private fun setupAttributes(attributeSet: AttributeSet)
+    {
+        val typedArray = context.theme.obtainStyledAttributes(attributeSet, R.styleable.ImageWithTextButton, 0, 0)
+
+        textView.text = typedArray.getString(R.styleable.ImageWithTextButton_text)
+        imageView.setImageResource(typedArray.getInteger(R.styleable.ImageWithTextButton_image,0))
+
+    }
+
+    private fun initViews()
+    {
+        var view = LayoutInflater.from(context).inflate(R.layout.image_with_text_button,this,false)
+        imageView = view.findViewById<ImageView>(R.id.button_image)
+        textView = view.findViewById<TextView>(R.id.button_text)
+    }
 }
